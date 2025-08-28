27fa3e85d45d287f4a39fe500be9a8e87f36e1b6
Shisir <ccrpc64@gmail.com>
2025-08-28 20:49:55 +0545

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,94 @@
+﻿using CRUDMVCProject.Data;
+using CRUDMVCProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+
+namespace CRUDMVCProject.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationContext context;
+        public EmployeeController(ApplicationContext context)
+        {
+            this.context = context;
+        }
+
+        public ApplicationContext Context { get; set; }
+        public string Name { get; private set; }
+        public object Salary { get; private set; }
+
+        public IActionResult Index()
+        {
+            var data = context.detail.ToList();
+            return View(data);
+        }
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee model)
+        {
+            if (ModelState.IsValid)
+            {
+                var data = new Employee
+
+                {
+                    Name = model.Name,
+                    Salary = model.Salary,
+                };
+                context.detail.Add(data);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+        public IActionResult Delete(int id)
+        {
+            var emp = context.detail.SingleOrDefault(x => x.Id == id);
+            context.detail.Remove(emp);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var emp = context.detail.SingleOrDefault(x => x.Id == id);
+            var result = new Employee();
+            {
+                Name = emp.Name;
+                Salary = emp.Salary;
+            }
+            return View(result);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Employee model)
+        {
+            if (ModelState.IsValid)
+            {
+                var data = new Employee
+
+                {
+                    Name = model.Name,
+                    Salary = model.Salary,
+                };
+                context.detail.Add(data);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                return View(model);
+            }
+
+        }
+
+    }
+}
---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using CRUDMVCProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDMVCProject.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        internal readonly object Employees;
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> detail { get; set; }
+        //public object Employee { get; internal set; }
+    }
+}
